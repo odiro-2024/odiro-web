8a0dd0e43b8e52821764ef47360fb21c0be197a3
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-05-04 07:04:00 +0900

    Feat: Add router function
    라우터 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Reset } from "styled-reset";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Reset />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
